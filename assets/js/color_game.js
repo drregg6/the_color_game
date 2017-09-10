029db12e143032b1b71a271be81ad20df2c9c390
Dave <drregg6@gmail.com>
2017-09-10 13:17:04 -0400

    preliminary winning screen complete

--- assets/js/color_game.js
+++ assets/js/color_game.js
@@ -1,17 +1,44 @@
+// newGame should start on pageload
+
+
+
 var startGame = document.querySelector('button');
 var choices = document.querySelectorAll('.choice');
 var winningColor = document.querySelector('h1');
+var header = document.querySelector('header');
 
 var colorsArr = [];
+var winningColorId = 0;
 
 startGame.addEventListener('click', function() {
+    header.style.backgroundColor = '#69acdd';
     colorsArr = [];
+    winningColorId = 0;
     
     choices.forEach(function(choice) {
+        choice.classList.remove('invisible');
+        
         var randColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
         choice.style.backgroundColor = randColor;
         colorsArr.push(randColor);
     });
     
-    winningColor.innerText = `${colorsArr[Math.floor(Math.random() * colorsArr.length)]}`;
+    winningColorId = Math.floor(Math.random() * colorsArr.length);
+    winningColor.innerText = `${colorsArr[winningColorId]}`;
+});
+
+choices.forEach(function(choice) {
+    choice.addEventListener('click', function() {
+        if (choice.getAttribute('id') === String(winningColorId)) {
+            console.log('You Win!');
+            choices.forEach(function(choice) {
+                choice.classList.remove('invisible');
+                choice.style.backgroundColor = colorsArr[winningColorId];
+            });
+            header.style.backgroundColor = colorsArr[winningColorId];
+        } else {
+            this.classList.add('invisible');
+            console.log('Keep trying...');
+        }
+    });
 });
