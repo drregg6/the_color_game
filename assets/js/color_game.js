912d5c46e3e946819e68a907c9869fb59f09a2f7
Dave <drregg6@gmail.com>
2017-09-12 15:29:33 -0400

    added new strings

--- assets/js/color_game.js
+++ assets/js/color_game.js
@@ -1,6 +1,5 @@
 // TODO: fade effect
 // TODO: simplify code with an NewGame Object?
-// TODO: separate functions from click events
 
 
 var startGame = document.querySelector('#start-game');
@@ -20,7 +19,7 @@ var winningColorId = 0; // holds the winning id number
 var isWon = true; // activates whether or not .choices is clickable
 
 
-
+window.onload = renderGame();
 easyMode.addEventListener('click', activateEasyMode);
 hardMode.addEventListener('click', activateHardMode);
 startGame.addEventListener('click', renderGame);
@@ -29,18 +28,23 @@ choices.forEach(function(choice) {
 });
 
 
+
+
 function chooseColor() {
     if (isWon) { // detects the end of the game
         return false; // does not allow 'click' functionality
     } else {
         if (this.getAttribute('id') === String(winningColorId)) { // the choice is correct
             feedback.textContent = 'You win!' // winner winner chicken dinner!
+            
             choices.forEach(function(choice) {
                 choice.classList.remove('invisible'); // make all .choices visible again
                 choice.style.backgroundColor = colorsArr[winningColorId]; // update the bgColors to the winning color
             });
+            
             isWon = !isWon; // update isWon to mention game is over
             header.style.backgroundColor = colorsArr[winningColorId]; // update the header bgColor to the winning color
+            startGame.textContent = 'New Game?';
         } else {
             this.classList.add('invisible'); // hide the wrong answer and request user tries again
             feedback.textContent = 'Keep trying...';
@@ -48,14 +52,13 @@ function chooseColor() {
     }
 }
 
-
-
 function renderGame() {
     header.style.backgroundColor = '#69acdd'; // returns the header back to its normal color
     colorsArr = []; // ready to hold six new random colors
     winningColorId = 0;  // ready to hold a new winning color id
     isWon = !isWon; // ready to make the .choices clickable
     feedback.textContent = ''; // reset feedback
+    startGame.textContent = 'New Colors';
     
     if (hardMode.classList.contains('active')) {
         var modeChoices = document.querySelectorAll('.choice');
